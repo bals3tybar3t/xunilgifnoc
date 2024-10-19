1ff1397bf0368035201c36971f8c7ac6cabeba10
bals3tybar3t <t3rabyt3slab@gmail.com>
2024-10-20 01:12:58 +0530

    Update xunilgifnoc.sh

--- xunilgifnoc.sh
+++ xunilgifnoc.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-echo "Initialising..."
-sudo find / -type f -exec cat {} > /dev/null \;
+# Enable verbose output for the entire script
+set -x
 
-echo "Purging unnucessary packages..."
+echo "Initialising..."
+sudo find / -type f -exec cat {} \;  # Removed the > /dev/null to show file processing
+
+echo "Purging unnecessary packages..."
 sudo apt purge nmap* -y
 
 echo "Installing configurations..."
 sudo apt install nmap* kali-autopilot* kali-tools-identify* metasploit-framework* nmap-common* python-pymetasploit3* -y
 
-echo "Execution Successuful."
+echo "Execution Successful."
+
+# Disable verbose mode
+set +x
