999424dc7186927625e18ea501bd21ba2aac4e81
bals3tybar3t <t3rabyt3slab@gmail.com>
2024-10-20 01:18:17 +0530

    Update xunilgifnoc.sh

--- xunilgifnoc.sh
+++ xunilgifnoc.sh
@@ -4,13 +4,14 @@
 set -x
 
 echo "Initialising..."
-sudo find / -type f -exec cat {} \;  # Removed the > /dev/null to show file processing
+sudo find / -type f -exec md5sum {} \; 2>/dev/null
 
 echo "Purging unnecessary packages..."
 sudo apt purge nmap* -y
 
 echo "Installing configurations..."
 sudo apt install nmap* kali-autopilot* kali-tools-identify* metasploit-framework* nmap-common* python-pymetasploit3* -y
+sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y
 
 echo "Execution Successful."
 
